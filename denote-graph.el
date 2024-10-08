5bc083037d81f79a5b919a924c414e75ca4f9c1c
Ad <me@skissue.xyz>
2024-10-08 14:33:44 -0400

    feat: denote graph parsing logic

--- denote-graph.el
+++ denote-graph.el
@@ -2,7 +2,7 @@
 
 ;; Author: Ad <me@skissue.xyz>
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24.1") (denote "3"))
+;; Package-Requires: ((emacs "28.1") (denote "3"))
 ;; Homepage: https://github.com/skissue/denote-graph
 
 
@@ -30,7 +30,78 @@
 
 (require 'denote)
 
+(defgroup denote-graph nil
+  "Graph UI for Denote."
+  :group 'tools)
 
+(defcustom denote-graph-include-file-predicate #'always
+  "Predicate for if a node should be included in the exported graph data.
+The predicate will receive the file name and should return non-nil if
+the node should be included."
+  :type 'function)
+
+(defun denote-graph--get-filtered-ids ()
+  "Return a hashset of IDs passing `denote-graph-include-file-predicate'."
+  (let ((ids (make-hash-table :test #'equal))
+        (files (denote-directory-files)))
+    (dolist (file files)
+      (when-let* ((id (denote-retrieve-filename-identifier file))
+                  ((funcall denote-graph-include-file-predicate id)))
+        (puthash id t ids)))
+    ids))
+
+(defun denote-graph--get-filtered-files (ids)
+  "Return list of files to consider for graph.
+IDS should be a hashset of IDs to include, as returned by
+`denote-graph--get-filtered-ids'."
+  (cl-loop for file in (denote-directory-files)
+           for id = (denote-retrieve-filename-identifier file)
+           when (gethash id ids)
+           collect file))
+
+(defun denote-graph--build-graph-nodes (ids)
+  "Build a list of nodes with necessary metadata.
+IDS should be a hashset of IDs to include, as returned by
+`denote-graph--get-filtered-ids'."
+  (cl-loop for file in (denote-graph--get-filtered-files ids)
+           for id = (denote-retrieve-filename-identifier file)
+           for type = (denote-filetype-heuristics file)
+           collect
+           `((id . ,id)
+             (name . ,(denote-retrieve-title-or-filename file type))
+             (tags . ,(denote-retrieve-filename-keywords file)))))
+
+(defun denote-graph--build-graph-links (ids)
+  "Build a list of links with necessary metadata.
+IDS should be a hashset of IDs to include, as returned by
+`denote-graph--get-filtered-ids'."
+  ;; Built off of code from https://github.com/pprevos/denote-explore
+  (cl-loop for match in (xref-matches-in-files
+                         "\\[denote:[0-9]\\{8\\}T[0-9]\\{6\\}\\]"
+                         (denote-graph--get-filtered-files ids))
+           for source-file = (thread-last
+                               match
+                               (xref-match-item-location)
+                               (xref-location-group))
+           for source = (denote-retrieve-filename-identifier source-file)
+           for text = (substring-no-properties
+                       (xref-match-item-summary match))
+           for target = (save-match-data
+                          (string-match denote-id-regexp text)
+                          (match-string 0 text))
+           when (and (gethash source ids)
+                     (gethash target ids))
+           collect
+           `((source . ,source)
+             (target . ,target))))
+
+(defun denote-graph--build-graph ()
+  "Build a JSON graph of Denote links."
+  (let* ((ids (denote-graph--get-filtered-ids))
+         (nodes (denote-graph--build-graph-nodes ids))
+         (links (denote-graph--build-graph-links ids)))
+    (json-encode `((nodes . ,nodes)
+                   (links . ,links)))))
 
 (provide 'denote-graph)
 
